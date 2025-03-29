9108d074147b95e5e756576d877ebaf835771fee
Risheet Lenka <risheetlenka01@gmail.com>
2025-03-28 22:36:01 -0700

    implemented model and graph placeholders in main.py

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+class Model(nn.Module):
+    def __init__(self):
+        raise NotImplementedError("Model class is not implemented")
+
+
+def graph():
+    raise NotImplementedError("Graph function is not implemented")
+
+torch.manual_seed(392)
+
+dataframe = pd.read_csv(r"california-housing-prices-dataset\housing.csv")
